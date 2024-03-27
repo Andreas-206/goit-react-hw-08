0b64f6a6c8c8e45cf41c68df502e56bc24ceb00d
Andreas-206 <puma_real@ukr.net>
2024-03-27 18:59:51 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Toaster } from 'react-hot-toast'
 import AppBar from '../AppBar/AppBar'
-import css from './Layout.module'
+import css from './Layout.module.css'
 
 export default function Layout({ children }) {
 	return (
