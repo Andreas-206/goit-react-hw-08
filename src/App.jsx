328d55dc5ceacfd2e037db52840a9373a1da19c2
Andreas-206 <puma_real@ukr.net>
2024-03-27 18:22:53 +0200

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
-import { useEffect } from 'react'
-import ContactForm from './components/ContactForm/ContactForm'
-import SearchBox from './components/SearchBox/SearchBox'
-import ContactList from './components/ContactList/ContactList'
-import { useSelector, useDispatch } from 'react-redux'
-import { selectContacts } from './redux/selectors'
-import { fetchContacts } from './redux/contactsOps'
+import { Suspense, lazy, useEffect } from 'react'
+// import ContactForm from './components/ContactForm/ContactForm'
+// import SearchBox from './components/SearchBox/SearchBox'
+// import ContactList from './components/ContactList/ContactList'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { selectContacts } from './redux/selectors'
+// import { fetchContacts } from './redux/contactsOps'
+import Layout from './components/Layout/Layout'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
-const App = () => {
-	const dispatch = useDispatch()
-	const contacts = useSelector(selectContacts)
+const HomePage = lazy(() => import('./pages/Home.jsx'))
+const LoginPage = lazy(() => import('./pages/LogIn.jsx'))
+const RegisterPage = lazy(() => import('./pages/Register.jsx'))
+const ContactsPage = lazy(() => import('./pages/Contacts.jsx'))
 
-	useEffect(() => {
-		dispatch(fetchContacts())
-	}, [dispatch])
+const App = () => 
 
 	return (
-		<div className='app-container'>
-			<h1 className='title'>Phonebook</h1>
-			<ContactForm />
-			{contacts.length === 0 ? <b>Add contact</b> : <SearchBox />}
-			<ContactList />
-		</div>
+		<Layout>
+			<Suspense fallback={null}>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/login' element={<LoginPage />} />
+					<Route path='/register' element={<RegisterPage />} />
+					<Route path='/contacts' element={<ContactsPage />} />
+				</Routes>
+			</Suspense>
+		</Layout>
 	)
 }
 
 export default App
+
+	// const dispatch = useDispatch()
+	// const contacts = useSelector(selectContacts)
+
+	// useEffect(() => {
+	// 	dispatch(fetchContacts())
+	// }, [dispatch])
+
+	{
+		/* <div className='app-container'>
+			<h1 className='title'>Phonebook</h1>
+			<ContactForm />
+			{contacts.length === 0 ? <b>Add contact</b> : <SearchBox />}
+			<ContactList />
+		</div> */
+	}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import PageTitle from '../components/PageTitle/PageTitle'
+
+const Home = () => {
+	return (
+		<div>
+			<PageTitle>Contacts Book</PageTitle>
+		</div>
+	)
+}
+
+export default Home
