a7aa7539b1c6ebbc6cd70c0a68298007dd8cc3b2
Andreas-206 <puma_real@ukr.net>
2024-03-31 20:22:12 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,60 @@
-import { Suspense, lazy } from 'react'
+import { Suspense, lazy, useEffect } from 'react'
 import Layout from './components/Layout/Layout'
 import { Routes, Route } from 'react-router-dom'
 import { RestrictedRoute } from './RestrictedRoute.jsx'
 import { PrivateRoute } from './PrivateRoute.jsx'
 import { useDispatch, useSelector } from 'react-redux'
+import { selectIsRefreshing } from './redux/auth/selectors.js'
+import { refreshUser } from './redux/auth/operations.js'
 import './App.css'
-import { useDispatch } from 'react-redux'
 
-const HomePage = lazy(() => import('./pages/Home.jsx'))
-const LoginPage = lazy(() => import('./pages/LogIn.jsx'))
-const RegisterPage = lazy(() => import('./pages/Register.jsx'))
-const ContactsPage = lazy(() => import('./pages/Contacts.jsx'))
+const Home = lazy(() => import('./pages/Home.jsx'))
+const Login = lazy(() => import('./pages/LogIn.jsx'))
+const Register = lazy(() => import('./pages/Register.jsx'))
+const Contacts = lazy(() => import('./pages/Contacts.jsx'))
 
 const App = () => {
+	const isRefreshing = useSelector(selectIsRefreshing)
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(refreshUser())
+	}, [dispatch])
+
 	return (
 		<Layout>
-			<Suspense fallback={null}>
-				<Routes>
-					<Route path='/' element={<HomePage />} />
-					<Route path='/login' element={<LoginPage />} />
-					<Route path='/register' element={<RegisterPage />} />
-					<Route path='/contacts' element={<ContactsPage />} />
-				</Routes>
-			</Suspense>
+			{isRefreshing ? (
+				<b>Refreshing user, please wait...</b>
+			) : (
+				<Suspense fallback={<>loding</>}>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route
+							path='/register'
+							element={
+								<RestrictedRoute
+									component={<Register />}
+									redirectTo='/contacts'
+								/>
+							}
+						/>
+						<Route
+							path='/login'
+							element={
+								<RestrictedRoute component={<Login />} redirectTo='/contacts' />
+							}
+						/>
+						<Route
+							path='/contacts'
+							element={
+								<PrivateRoute component={<Contacts />} redirectTo='/login' />
+							}
+						/>
+					</Routes>
+				</Suspense>
+			)}
 		</Layout>
 	)
 }
 
 export default App
-
-// const dispatch = useDispatch()
-// const contacts = useSelector(selectContacts)
-
-// useEffect(() => {
-// 	dispatch(fetchContacts())
-// }, [dispatch])
-
-{
-	/* <div className='app-container'>
-			<h1 className='title'>Phonebook</h1>
-			<ContactForm />
-			{contacts.length === 0 ? <b>Add contact</b> : <SearchBox />}
-			<ContactList />
-		</div> */
-}
