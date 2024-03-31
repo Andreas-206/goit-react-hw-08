c29ef1460035aa0b1226657ad9165b72d203b9c0
Andreas-206 <puma_real@ukr.net>
2024-03-31 20:45:41 +0300

    +

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import PageTitle from '../components/PageTitle/PageTitle'
+import ContactForm from '../components/ContactForm/ContactForm'
 import ContactList from '../components/ContactList/ContactList'
+import SearchBox from '../components/SearchBox/SearchBox'
+import { selectFilteredContacts } from '../redux/selectors'
 import { fetchContacts } from '../redux/contacts/contactsOps'
 import { selectLoading } from '../redux/selectors'
 
@@ -13,12 +16,16 @@ const Contacts = () => {
 		dispatch(fetchContacts())
 	}, [dispatch])
 
+	const contacts = useSelector(selectFilteredContacts)
+
 	return (
-		<>
+		<div className='wrap'>
 			<PageTitle>Your Contacts</PageTitle>
+			<ContactForm />
 			{isLoading && 'Request in progress...'}
+			{contacts.length === 0 ? <p>Add your first contact</p> : <SearchBox />}
 			<ContactList />
-		</>
+		</div>
 	)
 }
 
